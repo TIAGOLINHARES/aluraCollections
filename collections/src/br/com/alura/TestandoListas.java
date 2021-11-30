19a08f82ecbdded767ad8afa9e5a25ecc37d8a4b
Tiago <tiago.linhares@live.com>
2021-11-30 17:04:08 -0300

    01 trabalhando arrayList

---
+++ collections/src/br/com/alura/TestandoListas.java
@@ -0,0 +1,57 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestandoListas {
+
+	public static void main(String[] args) {
+		
+		String aula1 = "teste aula 1";
+		String aula2 = "teste aula 2";
+		String aula3 = "teste aula 3";
+		
+		ArrayList<String> aulas = new ArrayList<>();
+		
+		aulas.add(aula1);
+		aulas.add(aula2);
+		aulas.add(aula3);
+		
+		System.out.println(aulas);
+		
+		aulas.remove(0);
+		
+		System.out.println(aulas);
+		
+		for (String aula : aulas) {
+			
+			System.out.println("Aula: " + aula);
+			
+			
+		}
+		
+		String primairaAula = aulas.get(0);
+		
+		System.out.println(primairaAula);
+		
+		for(int i = 0; i < aulas.size();i++ ) {
+			System.out.println("Aula: " + aulas.get(i));
+		}
+		
+		System.out.println(aulas.size());
+		
+		aulas.forEach(aula -> {System.out.println("percorrendo:");
+		System.out.println("Aula: " + aula);
+		
+		
+		aulas.add("A teste de string");
+		System.out.println(aulas);
+		Collections.sort(aulas);
+		System.out.println(aulas);
+		
+		
+		
+		});
+	}
+
+}
