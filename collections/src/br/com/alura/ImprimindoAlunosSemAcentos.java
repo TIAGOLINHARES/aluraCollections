05c31282d4bc574d88c9dfd3ad876f15289d513c
Tiago <tiago.linhares@live.com>
2021-12-14 20:17:19 -0300

    O poder dos sets

---
+++ collections/src/br/com/alura/ImprimindoAlunosSemAcentos.java
@@ -0,0 +1,28 @@
+package br.com.alura;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ImprimindoAlunosSemAcentos {
+
+	public static void main(String[] args) {
+		
+		
+		Set<String> alunos = new HashSet<>();
+		
+		alunos.add("paulo");
+		alunos.add("Alberto");
+		alunos.add("Pedro");
+		alunos.add("Nico");
+		
+		
+		for (String aluno : alunos) {
+			
+			System.out.println(aluno);
+		}
+		
+		
+		
+	}
+
+}
---
+++ collections/src/br/com/alura/TesteAlunosSemAcentos.java
@@ -0,0 +1,25 @@
+package br.com.alura;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TesteAlunosSemAcentos {
+
+	public static void main(String[] args) {
+			
+			
+			Set<String> alunos = new HashSet<>();
+			
+			alunos.add("Tiago");
+			
+			System.out.println(alunos.size());
+			
+			alunos.add("Tiago");
+			
+			System.out.println(alunos.size());
+			
+			
+			
+		}
+
+}
